40a62a21d844941e64f7d754fd10ece91876581e
lupencia <lupegv83@gmail.com>
2018-11-13 23:10:53 +0100

    update budling webpack

--- students.js
+++ students.js
@@ -1,4 +1,9 @@
+import {getAvg} from './averageService';
+
+$('body').css('background-color', 'graylight');
+
 //let's use some ES6 features
+const scores = [90, 50, 40, 78, 60];
 const averageScore = "90";
 const messageToDisplay = `average score ${averageScore}`;
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,35 @@
+var HtmlWebpackplugin = require('html-webpack-plugin');
+var webpack =require('webpack');
+
 module.exports = {
-    entry: ['./students.js'],
+    entry: [
+        '@babel/polyfill',
+        './students.js'
+    ],
     output: {
         filename: 'bundle.js',
+    },
+    devServer: {
+        port:8081,
     },
     module: {
         rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: babel-loader,
+                loader: 'babel-loader',
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackplugin({
+            filename: 'index.html', //este está en dist
+            template: 'index.html',
+            hash: true,
+        }),
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+        })
+    ]
 }
